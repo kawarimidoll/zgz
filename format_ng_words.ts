e88c8316434c1d7d85e5cec3b0abd6244a938bc9
kawarimidoll <kawarimidoll+git@gmail.com>
2023-05-30 14:53:57 +0900

    feat: add format_ng_words

---
+++ format_ng_words.ts
@@ -0,0 +1,12 @@
+import { katakanaToHiragana } from "./hirakata_regexp.ts";
+
+function uniq(array: string[]) {
+  return [...new Set(array)];
+}
+
+const filename = "./ng_word_list.txt";
+const wordList = Deno.readTextFileSync(filename).split("\n").filter(
+  (word) => !/^\s*$/.test(word),
+).map((word) => katakanaToHiragana(word)).sort();
+
+Deno.writeTextFileSync(filename, uniq(wordList).join("\n"));
