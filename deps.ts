ba99a0757459062634261be150cff3123ceb38c9
kawarimidoll <kawarimidoll+git@gmail.com>
2023-06-12 10:06:18 +0900

    chore(deps): fix import/export strategy

--- deps.ts
+++ deps.ts
@@ -1,50 +1,25 @@
-import AtprotoAPI from "npm:@atproto/api";
-import type { Facet } from "npm:@atproto/api";
-
-import "https://deno.land/std@0.190.0/dotenv/load.ts";
+import "https://deno.land/std@0.191.0/dotenv/load.ts";
 
 // https://fukuno.jig.jp/3596
-import { TinySegmenter } from "https://raw.githubusercontent.com/kawarimidoll/TinySegmenter/patch-1/TinySegmenter.js";
+export { TinySegmenter } from "https://raw.githubusercontent.com/kawarimidoll/TinySegmenter/patch-1/TinySegmenter.js";
 
-import { partition } from "https://deno.land/std@0.190.0/collections/partition.ts";
-import { sample } from "https://deno.land/std@0.190.0/collections/sample.ts";
-import { chunk } from "https://deno.land/std@0.190.0/collections/chunk.ts";
-import {
+export { partition } from "https://deno.land/std@0.191.0/collections/partition.ts";
+export { sample } from "https://deno.land/std@0.191.0/collections/sample.ts";
+export { chunk } from "https://deno.land/std@0.191.0/collections/chunk.ts";
+export {
   assertEquals,
   assertExists,
-} from "https://deno.land/std@0.190.0/testing/asserts.ts";
-import {
+} from "https://deno.land/std@0.191.0/testing/asserts.ts";
+export {
   assertSpyCallArgs,
   assertSpyCalls,
   returnsNext,
   stub,
-} from "https://deno.land/std@0.190.0/testing/mock.ts";
+} from "https://deno.land/std@0.191.0/testing/mock.ts";
 
 import { Log } from "https://deno.land/x/tl_log@0.1.2/mod.ts";
-const log = new Log({ levelIndicator: "initial", color: false });
+export const log = new Log({ levelIndicator: "initial", color: false });
 
-import { TexTra } from "https://deno.land/x/tex_tra/tex_tra.ts";
+export { TexTra } from "https://deno.land/x/tex_tra@0.2.0/tex_tra.ts";
 
-const { BskyAgent, RichText, AppBskyFeedPost, AppBskyGraphListitem } =
-  AtprotoAPI;
-
-export {
-  AppBskyFeedPost,
-  AppBskyGraphListitem,
-  assertEquals,
-  assertExists,
-  assertSpyCallArgs,
-  assertSpyCalls,
-  BskyAgent,
-  chunk,
-  log,
-  partition,
-  returnsNext,
-  RichText,
-  sample,
-  stub,
-  TexTra,
-  TinySegmenter,
-};
-
-export type { Facet };
+export * from "./npm_deps.ts";
---
+++ npm_deps.ts
@@ -0,0 +1,8 @@
+import AtprotoAPI, { Facet } from "npm:@atproto/api";
+
+const { BskyAgent, RichText, AppBskyFeedPost, AppBskyGraphListitem } =
+  AtprotoAPI;
+
+export { AppBskyFeedPost, AppBskyGraphListitem, BskyAgent, RichText };
+
+export type { Facet };
